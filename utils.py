c2447b67e7158aa20afc5f62c41300d9ef109821
JackWardLT <larsntnu@gmail.com>
2024-08-18 23:14:30 +0200

    Made a utils folder for functions and comments on what to do tommorow.

---
+++ utils.py
@@ -0,0 +1,7 @@
+#Functions file
+
+#Function for parsing dir messages.
+#Do some for loop, where you take one line  and check if it has <DIR>. if it has <DIR> its a subdirectory which means we can get what ever the name is after.
+#Thentake what ever the the name is into list, re print the list for the user to choose which one application he wants to publish.
+#IT will insert the {application}/path-to-apk, then select the debugging file.
+#Maybe make some error handling if there are multiple apk files. like choose which one. same parsin process.
