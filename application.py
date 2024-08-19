b90c367abf501d8c2fd7c03115efc8a312d23b18
JackWardLT <larsntnu@gmail.com>
2024-08-19 18:49:49 +0200

    Removed the comments, started the desktop application

--- application.py
+++ application.py
@@ -3,5 +3,9 @@ from tkinter import ttk, filedialog
 
 
 window = Tk()
+window.title('SanBot Application Importer')
+
+#folderButton = tk.Button(window, text="Pick Folder", width=25, command=)
+#window.filename = filedialog.askdirectory(title="What is your folder for all your projects", initialdir="/")
 
 window.mainloop()
--- utils.py
+++ utils.py
@@ -2,24 +2,6 @@ import subprocess
 #Functions file
 rute = r"\app\build\outputs\apk\debug\app-debug.apk"
 
-
-#Function for parsing dir messages.
-#Do some for loop, where you take one line  and check if it has <DIR>. if it has <DIR> its a subdirectory which means we can get what ever the name is after.
-#Thentake what ever the the name is into list, re print the list for the user to choose which one application he wants to publish.
-#IT will insert the {application}/path-to-apk, then select the debugging file.
-#Maybe make some error handling if there are multiple apk files. like choose which one. same parsin process.
-
-
-# Sandbot confirmation
-
-#   Task: 
-# 1. Find if the robot is connected 
-# 2. upload the code based on the argument
-
-#   Resource: 
-# 1. adb devices  -> 40100100420caa8848cf
-# 2. path arg 
-
 def upload(path): 
     getDevices = subprocess.run("adb devices", capture_output=True, text=True, shell=True)
     devices = parseAdbDevices(getDevices.stdout)     
