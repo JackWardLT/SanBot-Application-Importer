5a77a8125fef9d03fcbb2e6e2b57378177285b71
JackWardLT <larsntnu@gmail.com>
2024-08-20 21:39:45 +0200

    Made a DIR button

--- application.py
+++ application.py
@@ -5,7 +5,19 @@ from tkinter import ttk, filedialog
 window = Tk()
 window.title('SanBot Application Importer')
 
-#folderButton = tk.Button(window, text="Pick Folder", width=25, command=)
-#window.filename = filedialog.askdirectory(title="What is your folder for all your projects", initialdir="/")
+def pickDirectory():
+    return (filedialog.askdirectory(title="What is your folder for all your projects", initialdir="/"))
+
+directoryButton = Button(window, text="Pick Folder", width=25, command=pickDirectory)
+
+header = Label(window, text="Choose a directory: ")
+
+
+
+header.grid(row=0, column=0, padx=100, pady=100)
+directoryButton.grid(row=0, column=1)
+
+
+
 
 window.mainloop()
