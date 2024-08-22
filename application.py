b59f6a8efadbe852262097b13356a507d3bc8e93
JackWardLT <larsntnu@gmail.com>
2024-08-22 22:47:21 +0200

    Experimenting with styling

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import ttk, filedialog
 from database import *
+import customtkinter
 
 def pickDirectory():
     print("pickDirectory function called\n")
@@ -12,14 +13,17 @@ def pickDirectory():
         fetched_data = fetchPathData('DIR')
         print(f"Fetched data: {fetched_data}\n")
 
-window = Tk()
+# Initialize customtkinter
+customtkinter.set_appearance_mode("System")  # Modes: "System" (default), "Dark", "Light"
+customtkinter.set_default_color_theme("blue")  # Themes: "blue" (default), "green", "dark-blue"
+
+window = customtkinter.CTk()  # Use CTk instead of Tk
 window.title('SanBot Application Importer')
 
-directoryButton = Button(window, text="Pick Folder", width=25, command=pickDirectory)
-header = Label(window, text="Choose a directory: ")
+directoryButton = customtkinter.CTkButton(window, text="Pick Folder", width=200, command=pickDirectory)
+header = customtkinter.CTkLabel(window, text="Choose a directory: ")
 
 header.grid(row=0, column=0, padx=100, pady=100)
 directoryButton.grid(row=0, column=1)
 
-window.protocol("WM_DELETE_WINDOW", lambda: (closeConnection(), window.destroy()))
 window.mainloop()
